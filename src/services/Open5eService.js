08c312eaf862fb77361e91701d8d555563e64ed6
James Morelli <morellijames12@gmail.com>
2021-06-15 18:08:27 -0400

    Refactor service to generic

--- src/services/Open5eService.js
+++ src/services/Open5eService.js
@@ -2,38 +2,8 @@ import axios from 'axios';
 
 const baseUrl = process.env.REACT_APP_OPEN5E_BASE_URL
 
-export const getMagicItems = (page) => {
-    return axios.get(`${baseUrl}/magicitems?page=${page}`)
-    .then(res => {
-        return res.data;
-    }).catch(err => {
-        console.log(err);
-        return err;
-    });
-}
-
-export const getSpells = (page) => {
-    return axios.get(`${baseUrl}/spells?page=${page}`)
-    .then(res => {
-        return res.data;
-    }).catch(err => {
-        console.log(err);
-        return err;
-    });
-}
-
-export const getMonsters = (page) => {
-    return axios.get(`${baseUrl}/monsters?page=${page}`)
-    .then(res => {
-        return res.data;
-    }).catch(err => {
-        console.log(err);
-        return err;
-    });
-}
-
-export const getWeapons = (page) => {
-    return axios.get(`${baseUrl}/weapons?page=${page}`)
+export const getData = (path, size) => {
+    return axios.get(`${baseUrl}${path}?size=${size}`)
     .then(res => {
         return res.data;
     }).catch(err => {
