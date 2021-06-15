fd921913249588fd5ec778d77bb5ec39fe0ce9ff
James Morelli <morellijames12@gmail.com>
2021-06-14 23:23:18 -0400

    Add GET calls #spells #monsters #weapons

--- src/services/Open5eService.js
+++ src/services/Open5eService.js
@@ -1,7 +1,39 @@
 import axios from 'axios';
 
+const baseUrl = process.env.REACT_APP_OPEN5E_BASE_URL
+
 export const getMagicItems = (page) => {
-    return axios.get(`https://api.open5e.com/magicitems?page=${page}`)
+    return axios.get(`${baseUrl}/magicitems?page=${page}`)
+    .then(res => {
+        return res.data;
+    }).catch(err => {
+        console.log(err);
+        return err;
+    });
+}
+
+export const getSpells = (page) => {
+    return axios.get(`${baseUrl}/spells?page=${page}`)
+    .then(res => {
+        return res.data;
+    }).catch(err => {
+        console.log(err);
+        return err;
+    });
+}
+
+export const getMonsters = (page) => {
+    return axios.get(`${baseUrl}/monsters?page=${page}`)
+    .then(res => {
+        return res.data;
+    }).catch(err => {
+        console.log(err);
+        return err;
+    });
+}
+
+export const getWeapons = (page) => {
+    return axios.get(`${baseUrl}/weapons?page=${page}`)
     .then(res => {
         return res.data;
     }).catch(err => {
