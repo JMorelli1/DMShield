6d63c2782df775599f51b7c3d5fef5bf2821c354
James Morelli <morellijames12@gmail.com>
2021-06-14 23:06:31 -0400

    Refactoring

--- src/services/Open5eService.js
+++ src/services/Open5eService.js
@@ -1,3 +1,11 @@
-export const getAllSpells = () => {
-    
+import axios from 'axios';
+
+export const getMagicItems = (page) => {
+    return axios.get(`https://api.open5e.com/magicitems?page=${page}`)
+    .then(res => {
+        return res.data;
+    }).catch(err => {
+        console.log(err);
+        return err;
+    });
 }
