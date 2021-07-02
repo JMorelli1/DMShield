d705c925fd3d8a57fd8aabcd871be2eef9cb8672
James Morelli <morellijames12@gmail.com>
2021-07-02 14:31:12 -0400

    Minor fix

--- src/services/Open5eService.js
+++ src/services/Open5eService.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const baseUrl = process.env.REACT_APP_OPEN5E_BASE_URL;
 
 export const getData = (path, size) => {
-<<<<<<< HEAD
   return axios
     .get(`${baseUrl}${path}?limit=${size}`)
     .then((res) => {
@@ -26,13 +25,3 @@ export const getSpell = (spell) => {
       return err;
     });
 };
-=======
-    return axios.get(`${baseUrl}${path}?size=${size}`)
-    .then(res => {
-        return res.data;
-    }).catch(err => {
-        console.log(err);
-        return err;
-    });
-}
->>>>>>> master
